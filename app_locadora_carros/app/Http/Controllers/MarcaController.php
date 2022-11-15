810ce64f6e1351c639be30235147d17fe6acc664
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-11-15 15:10:47 -0300

    Selecionando registros via GET

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -14,7 +14,9 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        $marcas = Marca::all();
+
+        return $marcas;
     }
 
     /**
@@ -48,7 +50,7 @@ class MarcaController extends Controller
      */
     public function show(Marca $marca)
     {
-        //
+        return $marca;
     }
 
     /**
