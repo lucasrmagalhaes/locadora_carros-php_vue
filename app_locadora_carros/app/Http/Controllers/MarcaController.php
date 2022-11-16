f5bb0b5af2a9447e74790f9c5b369f2515d60e7b
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-11-15 21:00:34 -0300

    Validação no Update - Lidando com o PUT/PATCH

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -70,7 +70,19 @@ class MarcaController extends Controller
             return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe!'], 404);
         }
 
-        $request->validate($marca->rules(), $marca->feedback());
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            foreach ($marca->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
 
         $marca->update($request->all());
 
