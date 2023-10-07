f41f722936077e65537e8a963e98f1a1562f5cd4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-10-07 09:07:26 -0300

    Implementando um Repository para Marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Repositories\MarcaRepository;
 use App\Models\Marca;
 
 class MarcaController extends Controller
 {
+
     public function __construct(Marca $marca)
     {
         $this->marca = $marca;
@@ -20,33 +22,24 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-        $marcas = $this->marca->all();
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if ($request->has('atributos_modelos')) {
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,' . $atributos_modelos);
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if ($request->has('filtro')) {
-            $filtros = explode(';', $request->filtro);
-
-            foreach ($filtros as $key => $condicao) {
-                $c = explode(':', $condicao);
-
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if ($request->has('atributos')) {
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($request->atributos);
         }
 
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     /**
