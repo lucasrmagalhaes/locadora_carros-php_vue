851a84636430843dd022b4fc9136f6e85a62a1ac
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-10-07 09:20:56 -0300

    Implementando um Abstract Repository

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -2,34 +2,4 @@
 
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-class MarcaRepository {
-
-    public function __construct(Model $model) {
-        $this->model = $model;
-    }
-
-    public function selectAtributosRegistrosRelacionados(string $atributos) {
-        $this->model = $this->model->with($atributos);
-    }
-
-    public function filtro($filtro) {
-        $filtros = explode(';', $filtro);
-
-        foreach ($filtros as $key => $condicao) {
-            $c = explode(':', $condicao);
-
-            $this->model = $this->model->where($c[0], $c[1], $c[2]);
-        }
-    }
-
-    public function selectAtributos($atributos) {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado() {
-        return $this->model->get();
-    }
-
-}
+class MarcaRepository extends AbstractRepository {}
