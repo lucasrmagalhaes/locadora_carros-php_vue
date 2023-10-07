cd7093425c74ef0e03ef58ca6c43ef32f911f56a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-10-06 22:56:05 -0300

    Aplicando condições nas pesquisas parte 2

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -30,8 +30,13 @@ class ModeloController extends Controller
         }
 
         if ($request->has('filtro')) {
-            $condicoes = explode(':', $request->filtro);
-            $modelos = $modelos->where($condicoes[0], $condicoes[1], $condicoes[2]);
+            $filtros = explode(';', $request->filtro);
+
+            foreach ($filtros as $key => $condicao) {
+                $c = explode(':', $condicao);
+
+                $modelos = $modelos->where($c[0], $c[1], $c[2]);
+            }
         }
 
         if ($request->has('atributos')) {
