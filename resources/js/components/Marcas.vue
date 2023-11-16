9c721c69758b23bd961e654fd0dd36f145f4c0a5
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-11-16 00:23:14 -0300

    Add botões visualização, atualização e remoção

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -62,6 +62,9 @@
                     <template v-slot:conteudo>
                         <table-component
                             :dados="marcas.data"
+                            :visualizar="true"
+                            :atualizar="true"
+                            :remover="true"
                             :titulos="{
                                 id: { titulo: 'ID', tipo: 'texto' },
                                 nome: { titulo: 'Nome', tipo: 'texto' },
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -6,6 +6,8 @@
                     <th v-for="value, key in titulos" :key="key" scope="col">
                         {{ value.titulo }}
                     </th>
+
+                    <th v-if="visualizar || atualizar || remover"></th>
                 </tr>
             </thead>
 
@@ -24,6 +26,12 @@
                             <img :src="'/storage/' + valor" width="30" height="30" />
                         </span>
                     </td>
+
+                    <td v-if="visualizar || atualizar || remover">
+                        <button v-if="visualizar" class="btn btn-outline-primary btn-sm">Visualizar</button>
+                        <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                        <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -32,7 +40,7 @@
 
 <script>
     export default {
-        props: ['dados', 'titulos'],
+        props: ['dados', 'titulos', 'atualizar', 'visualizar', 'remover'],
         computed: {
             dadosFiltrados() {
                 let campos = Object.keys(this.titulos);
